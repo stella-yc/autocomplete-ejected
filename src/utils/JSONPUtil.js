0679e97ba4ce05633bd0e93378676c78fc0c2c39
Stella Chung <stll.chung@gmail.com>
2017-11-14 22:26:11 -0500

    Created utils folder and included JSONPUtil.js file

---
+++ src/utils/JSONPUtil.js
@@ -0,0 +1,33 @@
+// Borrowed gratefully from Shoefitr.com
+var JSONPUtil = {
+
+      _oJsonpRequests: {},
+
+      _iJsonpRequestCount: 0,
+
+      DispatchJsonpResponse: function(oResponse, sEcho) {
+          // sEcho contains the request id
+          var callback = this._oJsonpRequests[sEcho];
+          delete this._oJsonpRequests[sEcho];
+
+          if (callback) {
+              callback(oResponse);
+          }
+      },
+
+      // Note that query string variables 'callback' and 'echo' are appended to sUrl, so your request URL
+      // must not contain either of these. Also be sure to append "?sid=" + Math.random() to the URL to avoid
+      // cache hits.
+      LoadJSONP: function(sUrl, f) {
+          // sEcho contains the request id
+          var sEcho = this._iJsonpRequestCount++;
+          this._oJsonpRequests[sEcho] = f;
+
+          var script = document.createElement('script');
+          script.setAttribute('src', sUrl +
+              '&callback=JSONPUtil.DispatchJsonpResponse' +
+              '&echo=' + sEcho
+          );
+          document.body.appendChild(script);
+      }
+  };
