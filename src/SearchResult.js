efd4cec9b97a52c1d13cf9b8b7ffb2cceecde391
Stella Chung <stll.chung@gmail.com>
2017-11-15 21:08:11 -0500

    Bold part of name that matches typedValue for each search result

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -4,10 +4,38 @@ import React, { Component } from 'react';
 const SearchResult = (props) => {
   const { typedValue, result } = props;
   const { name, url, location, id } = result;
+
+  const substringIdx = (sub, text) => {
+    const lcSub = sub.toLowerCase();
+    const lcText = text.toLowerCase();
+    return lcText.indexOf(lcSub);
+  };
+
+  const formattedElement = (str, start, end) => (
+    <span>
+      {str.slice(0, start)}
+      <strong>
+      {str.slice(start, end)}
+      </strong>
+      {str.slice(end)}
+  </span>
+  );
+
+  const boldMatchingText = (highlight, text) => {
+    const startIdx = substringIdx(highlight, text);
+    if (startIdx === -1) {
+      return (
+        <span>{text}</span>
+      );
+    }
+    return formattedElement(text, startIdx, startIdx + highlight.length);
+  };
+
     return (
       <li>
         <a href={url} >
-          {name}
+          { boldMatchingText(typedValue, name) }
+          <small>{location}</small>
         </a>
       </li>
     );
