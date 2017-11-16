2cd8976885ad0d9de246005d16c654ea6be07644
Stella Chung <stll.chung@gmail.com>
2017-11-16 09:44:07 -0500

    When selected and navigateToSelected is true, component will programmatically click on link to college profile.

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import './SearchResult.css';
 
-const SearchResult = (props) => {
-  const { typedValue, result, selected } = props;
-  const { name, url, location, id } = result;
+class SearchResult extends Component {
+  constructor(props) {
+    super(props);
+    this.bindElement = this.bindElement.bind(this);
+    this.navigateToLink = this.navigateToLink.bind(this);
+  }
 
-  const substringIdx = (sub, text) => {
+  substringIdx (sub, text) {
     const lcSub = sub.toLowerCase();
     const lcText = text.toLowerCase();
     return lcText.indexOf(lcSub);
-  };
+  }
 
-  const formattedElement = (str, start, end) => (
+  formattedElement (str, start, end) {
+    return (
     <span>
       {str.slice(0, start)}
       <strong>
@@ -19,27 +23,55 @@ const SearchResult = (props) => {
       </strong>
       {str.slice(end)}
   </span>
-  );
+  )}
 
-  const boldMatchingText = (highlight, text) => {
-    const startIdx = substringIdx(highlight, text);
+  boldMatchingText (highlight, text) {
+    const startIdx = this.substringIdx(highlight, text);
     if (startIdx === -1) {
       return (
         <span>{text}</span>
       );
     }
-    return formattedElement(text, startIdx, startIdx + highlight.length);
-  };
+    return this.formattedElement(text, startIdx, startIdx + highlight.length);
+  }
 
+  navigateToLink (selected, goToLink) {
+    this.link.click();
+  }
+
+  bindElement (el) {
+    this.link = el;
+    const { selected, goToLink } = this.props;
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.selected && nextProps.goToLink) {
+      this.navigateToLink(nextProps.selected, nextProps.goToLink);
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.goToLink && !nextProps.selected) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  render () {
+    const { typedValue, result, selected, goToLink } = this.props;
+    const { name, url, location, id } = result;
     return (
       <li className={`SearchResult ${selected ? 'SearchResult-selected' : ''}`}>
-        <a href={url} >
-          { boldMatchingText(typedValue, name) }
+        <a
+          ref={this.bindElement}
+          href={url} >
+          { this.boldMatchingText(typedValue, name) }
           <small>{location}</small>
         </a>
       </li>
     );
-
+  }
 }
 
 export default SearchResult;
