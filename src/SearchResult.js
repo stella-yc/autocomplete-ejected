19f174e3db810c45d180151fb2bc626f7df04458
Stella Chung <stll.chung@gmail.com>
2017-11-19 13:17:56 -0500

    Added PropTypes, organized methods, clarified className generation for selected component

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import './SearchResultV1.css';
 import './SearchResultV2.css';
 
@@ -7,6 +9,17 @@ class SearchResult extends Component {
     super(props);
     this.bindElement = this.bindElement.bind(this);
     this.navigateToLink = this.navigateToLink.bind(this);
+  }
+
+  //highlight portion of result name that matches typedValue
+  boldMatchingText (highlight, text) {
+    const startIdx = this.substringIdx(highlight, text);
+    if (startIdx === -1) {
+      return (
+        <span>{text}</span>
+      );
+    }
+    return this.formattedElement(text, startIdx, startIdx + highlight.length);
   }
 
   substringIdx (sub, text) {
@@ -26,32 +39,24 @@ class SearchResult extends Component {
   </span>
   )}
 
-  boldMatchingText (highlight, text) {
-    const startIdx = this.substringIdx(highlight, text);
-    if (startIdx === -1) {
-      return (
-        <span>{text}</span>
-      );
-    }
-    return this.formattedElement(text, startIdx, startIdx + highlight.length);
-  }
-
-  navigateToLink (selected, goToLink) {
-    this.link.click();
-  }
-
+  // passed to ref "escape hatch"
   bindElement (el) {
     this.link = el;
     const { selected, goToLink } = this.props;
   }
 
-  componentWillReceiveProps(nextProps) {
+  // allows us to programmatically click on a link
+  navigateToLink (selected, goToLink) {
+    this.link.click();
+  }
+
+  componentWillReceiveProps (nextProps) {
     if (nextProps.selected && nextProps.goToLink) {
       this.navigateToLink(nextProps.selected, nextProps.goToLink);
     }
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
+  shouldComponentUpdate (nextProps, nextState) {
     if (nextProps.goToLink && !nextProps.selected) {
       return false;
     } else {
@@ -61,14 +66,28 @@ class SearchResult extends Component {
 
   render () {
     const { typedValue, result, selected, goToLink, style } = this.props;
-    const { name, url, location, id } = result;
+    const { name, url, location } = result;
     return (
-      <li className={`SearchResult-${style} ${selected ? `SearchResult-selected-${style}` : ''}`}>
+      <li
+        className={
+          `SearchResult-${style}
+          ${selected ? `SearchResult-selected-${style}` : ''}`
+        }
+      >
         <a
           ref={this.bindElement}
           href={url} >
-          { this.boldMatchingText(typedValue, name) }
-          <small className={`SearchResult-small${selected ? '-selected' : ''}-${style}`}>{location}</small>
+          {
+            this.boldMatchingText(typedValue, name)
+          }
+          <small
+            className={
+              `SearchResult-small-${style}
+              ${selected ? `SearchResult-small-selected-${style}` : ''}`
+            }
+          >
+            { location }
+          </small>
         </a>
       </li>
     );
@@ -76,3 +95,17 @@ class SearchResult extends Component {
 }
 
 export default SearchResult;
+
+SearchResult.propTypes = {
+  style: PropTypes.oneOf(['v1', 'v2']),
+  goToLink: PropTypes.bool.isRequired,
+  selected: PropTypes.bool.isRequired,
+  result: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    location: PropTypes.string,
+    id: PropTypes.string
+  }),
+  typedValue: PropTypes.string.isRequired
+};
+
