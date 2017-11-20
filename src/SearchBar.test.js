89d07f8c92c3696bc86559f0f4ada8a0d08eef8a
Stella Chung <stll.chung@gmail.com>
2017-11-20 15:48:09 -0500

    Unit tests for SearchBar Component

--- src/SearchBar.test.js
+++ src/SearchBar.test.js
@@ -1,39 +1,189 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
-import SearchBar from './SearchBar';
 import renderer from 'react-test-renderer';
+import { shallow, mount } from 'enzyme';
 
-it('renders shallowly without crashing', () => {
-  shallow(<SearchBar />);
-});
+import SearchBar from './SearchBar';
+import retrieveSearchResults from './async-request/searchQueryAPI';
+import SearchResult from './SearchResult';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<SearchBar />, div);
-});
+describe('<SearchBar /> Component', () => {
 
-it('renders correctly', () => {
-  const tree = renderer.create(
-    <SearchBar />
-  ).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+  const result = [
+    { name: 'Brown',
+    location: 'Providence',
+    url: "brown.edu",
+    id: '1'},
+    { name: 'Bro',
+    location: 'Broland',
+    url: 'bro.edu',
+    id: '2'}
+  ];
+  // mockFn will serve as spy/mock for retrieveSearchResults
+  // mockFn must be a promise since we use .then api in SearchBar component
+  // mockFn will return a promise that returns the given result
+  const mockFn = () => Promise.resolve(result);
 
-it('updates state when user types into input', () => {});
+  it('renders shallowly without crashing', () => {
+    shallow(
+      <SearchBar
+        retrieveSearchResults={mockFn}
+      />);
+  });
 
-it('queries api when user types into input', () => {});
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <SearchBar
+        retrieveSearchResults={mockFn}
+      />, div);
+  });
 
-it('updates state when up arrow key is pressed on input', () => {});
+  it('renders correctly', () => {
+    const tree = renderer.create(
+      <SearchBar
+        retrieveSearchResults={mockFn}
+      />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-it('updates state when up down key is pressed on input', () => {});
+  it('updates state when user types into input', () => {
+    const wrapper = shallow(
+      <SearchBar
+        retrieveSearchResults={mockFn}
+      />
+    );
+    const typed = 'brown';
+    wrapper
+      .find('input')
+      .simulate('change', {target: { value: typed }});
+    expect(wrapper.state().typedValue).toBe(typed);
+  });
 
-it('updates state when up enter key is pressed on input', () => {});
+  it('queries api when user types into input', async () => {
+    // note we are using async/await
+    const wrapper = shallow(
+      <SearchBar
+        style="v1"
+        retrieveSearchResults={mockFn}
+      />
+    );
+    wrapper
+      .find('input')
+      .simulate('change', {target: { value: 'b' }});
+    await mockFn // we wait for the promise to resolve first
+    const wrapperResult = wrapper.state().results[0];
+    // .toEqual checks deep equality of objects,
+    // unlike .toBe which uses strict equality
+    expect(wrapperResult).toEqual(result[0]);
+  });
 
-it('if no style is provided, default to v1', () => {});
+  it('updates state when up/down arrow key is pressed on input', async () => {
+    // note we are using async/await
+    const wrapper = shallow(
+      <SearchBar
+        style="v1"
+        retrieveSearchResults={mockFn}
+      />
+    );
+    const wrapperInput = wrapper.find('input');
+    wrapperInput
+      .simulate('change', {target: { value: 'b' }});
+    await mockFn // we wait for the promise to resolve first
 
-it('if supplied with style, classNames reflect style', () => {});
+    // now we have results on state
+    expect(wrapper.state().results.length).toBe(2);
 
-it('if there are no search results, ul element is absent', () => {});
+    // events for up arrow and down arrow
+    const eventUp = { keyCode: 38 };
+    const eventDown = { keyCode: 40 };
 
-it('if there are search results, SearchResult component(s) is present', () => {});
+    // simulate arrow keys
+    expect(wrapper.state().selected).toBe(0);
+    wrapperInput
+      .simulate('keyDown', eventDown);
+    expect(wrapper.state().selected).toBe(1);
+    wrapperInput
+      .simulate('keyDown', eventDown);
+    expect(wrapper.state().selected).toBe(2);
+    wrapperInput
+      .simulate('keyDown', eventUp);
+    expect(wrapper.state().selected).toBe(1);
+  });
+
+  it('updates state when enter key is pressed on input', async () => {
+    // note we are using async/await
+    const wrapper = shallow(
+      <SearchBar
+        style="v1"
+        retrieveSearchResults={mockFn}
+      />
+    );
+    const wrapperInput = wrapper.find('input');
+    wrapperInput
+      .simulate('change', {target: { value: 'b' }});
+    await mockFn // we wait for the promise to resolve first
+
+    expect(wrapper.state().navigateToSelected).toBe(false);
+    const eventEnter = { keyCode: 13 };
+    wrapperInput
+      .simulate('keyDown', eventEnter);
+    expect(wrapper.state().navigateToSelected).toBe(true);
+  });
+
+  it('if no style is provided, default to v1', () => {
+    const wrapper = shallow(
+      <SearchBar
+        retrieveSearchResults={mockFn}
+      />
+    );
+    expect(wrapper.find('.Searchbar-component-v1')).toHaveLength(1);
+    expect(wrapper.find('.Searchbar-component-v2')).toHaveLength(0);
+  });
+
+  it('if supplied with style, classNames reflect style', () => {
+    const wrapper = shallow(
+      <SearchBar
+        style="v2"
+        retrieveSearchResults={mockFn}
+      />
+    );
+    expect(wrapper.find('.Searchbar-component-v1')).toHaveLength(0);
+    expect(wrapper.find('.Searchbar-component-v1').exists()).toBe(false);
+    expect(wrapper.find('.Searchbar-component-v2')).toHaveLength(1);
+  });
+
+  it('if there are no search results, ul element is absent', () => {
+    const wrapper = shallow(
+      <SearchBar
+        retrieveSearchResults={() => true}
+      />
+    );
+    expect(wrapper.find('.Searchbar-results-v1')).toHaveLength(0);
+    expect(wrapper.find('.Searchbar-results-v1').exists()).toBe(false);
+  });
+
+  it('if there are search results, SearchResult component(s) is present', async () => {
+    // note we are using async/await
+    const wrapper = mount(
+      <SearchBar
+        retrieveSearchResults={mockFn}
+      />
+    );
+    const wrapperInput = wrapper.find('input');
+    wrapperInput
+      .simulate('change', {target: { value: 'b' }});
+    await mockFn // we wait for the promise to resolve first
+
+    expect(wrapper.find(SearchResult)).toHaveLength(0);
+    expect(wrapper.find('.Searchbar-results-v1').exists()).toBe(false);
+
+    wrapper.update(); // forces a re-render of component
+
+    expect(wrapper.state().results).toHaveLength(2);
+    expect(wrapper.find(SearchResult)).toHaveLength(2);
+    expect(wrapper.find('.Searchbar-results-v1').exists()).toBe(true);
+  });
+
+})
